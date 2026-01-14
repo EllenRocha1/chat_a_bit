c03102cde652095191726e4c92285a9ef14ad9b5
Ellen Rocha <ellenthaisy52@gmail.com>
2026-01-14 01:51:24 -0300

    Implementação do executável com Splash Screen e correção de caminhos

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import customtkinter as ctk
 from interface.login import criar_tela_login
 from utils.path import resource_path
+import os
 
 if __name__ == "__main__":
+    try:
+        import pyi_splash # type: ignore
+        pyi_splash.close()
+    except ImportError:
+        pass
+
     ctk.set_appearance_mode("dark")
     app = ctk.CTk()
     app.title("Chat a Bit")
--- utils/path.py
+++ utils/path.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 def resource_path(relative_path):
-    """ Retorna o caminho absoluto para o recurso, funciona no dev e no PyInstaller """
     try:
-        # O PyInstaller cria uma pasta temporária e armazena o caminho em _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
-        base_path = resource_path(".")
+        # Mude de resource_path(".") para os.path.abspath(".")
+        base_path = os.path.abspath(".") 
     return os.path.join(base_path, relative_path)
+
